e0bd9ba0e061af26ac2ec0f6433156d93d6d1f54
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-12-21 07:17:12 -0400

    Pasando datos estaticos a datos dinamicos

--- src/Invie.js
+++ src/Invie.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
 import './css/invie.css';
-import Portada from './components/Portada'
-import Guitarras from './components/Guitarras'
-import Footer from './components/Footer'
+import Portada from './components/Portada';
+import Guitarras from './components/Guitarras';
+import Footer from './components/Footer';
+import logoPortada from './images/invie.png';
+import acustica from './images/invie-acustica.png';
+import clasica from './images/invie-classic.png';
+
+const data = {
+  menu: 
+  [
+    {
+      href: 'index.html',
+      title: 'Home',
+    },
+    {
+      href: '#guitarras',
+      title: 'Guitarras',
+    },
+    {
+      href: 'precios.html',
+      title: 'Precios',
+    }
+  ],
+  logoPortada: {
+    src: logoPortada,
+    alt: 'Invie logotipo'
+  },
+  guitarras: [
+    {
+      image: acustica,
+      alt: 'Guitarra Invie Acústica',
+      name: 'Invie Acústica',
+      features: [
+        'Estilo vintage',
+        'Madera pura',
+        'Incluye estuche invisible de aluminio',
+      ]
+    },
+    {
+      image: clasica,
+      alt: 'Guitarra Invie Classic',
+      name: 'Invie Classic',
+      features: [
+        'Estilo vintage',
+        'Liviana',
+        'Empieza tu camino como Rockstar',
+      ]
+    },
+  ]
+};
+
 
 class Invie extends Component {
   render() {
     return (
       <section className="Invie">
         {/*Portada*/}
-        <Portada />
+        <Portada menu={data.menu} logo={data.logoPortada} />
         {/*Guitarras*/}
-        <Guitarras />
+        <Guitarras guitarras={data.guitarras} />
         {/*Footer*/}
         <Footer />
       </section>
--- src/components/Portada.js
+++ src/components/Portada.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from '../images/invie.png'
 
 class Portada extends Component {
   render() {
@@ -7,20 +6,18 @@ class Portada extends Component {
       <section id="portada" className="portada background">
         <header id="header" className="header contenedor">
             <figure className="logotipo">
-                <img src={logo} width="186" height="60" alt="Invie logotipo" />
+                <img src={this.props.logo.src} width="186" height="60" alt={this.props.logo.alt} />
             </figure>
             <span id="burguer-button" className="burguer-button icon-menu"></span>
             <nav id="menu" className="menu">
                 <ul>
-                    <li>
-                        <a href="index.html">Home</a>
+                {this.props.menu.map((item, index) => {
+                  return (
+                    <li key={index}>
+                        <a href={item.href}>{item.title}</a>
                     </li>
-                    <li>
-                        <a href="#guitarras">Guitarras</a>
-                    </li>
-                    <li>
-                        <a href="precios.html">Precios</a>
-                    </li>
+                  )
+                })}
                 </ul>
             </nav>
         </header>
