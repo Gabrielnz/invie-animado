1d39b9fe3e2cab5be9b17bf70fe10289ff7b07be
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-12-22 20:18:49 -0400

    Configurando Redux

--- src/Invie.js
+++ src/Invie.js
@@ -3,73 +3,15 @@ import './css/invie.css';
 import Portada from './components/Portada';
 import Guitarras from './components/Guitarras';
 import Footer from './components/Footer';
-import logoPortada from './images/invie.png';
-import acustica from './images/invie-acustica.png';
-import clasica from './images/invie-classic.png';
-
-import cheet from 'cheet.js';
-
-cheet('i n v i e', () => {
-  console.log('Easter Egg!! :D');
-});
-
-cheet('g o b a c k', () => {
-  console.log('Regresaste al estado inicial');
-})
-
-const data = {
-  menu: 
-  [
-    {
-      href: 'index.html',
-      title: 'Home',
-    },
-    {
-      href: '#guitarras',
-      title: 'Guitarras',
-    },
-    {
-      href: 'precios.html',
-      title: 'Precios',
-    }
-  ],
-  logoPortada: {
-    src: logoPortada,
-    alt: 'Invie logotipo'
-  },
-  guitarras: [
-    {
-      image: acustica,
-      alt: 'Guitarra Invie Acústica',
-      name: 'Invie Acústica',
-      features: [
-        'Estilo vintage',
-        'Madera pura',
-        'Incluye estuche invisible de aluminio',
-      ]
-    },
-    {
-      image: clasica,
-      alt: 'Guitarra Invie Classic',
-      name: 'Invie Classic',
-      features: [
-        'Estilo vintage',
-        'Liviana',
-        'Empieza tu camino como Rockstar',
-      ]
-    },
-  ]
-};
-
 
 class Invie extends Component {
   render() {
     return (
       <section className="Invie">
         {/*Portada*/}
-        <Portada menu={data.menu} logo={data.logoPortada} />
+        <Portada />
         {/*Guitarras*/}
-        <Guitarras guitarras={data.guitarras} />
+        <Guitarras />
         {/*Footer*/}
         <Footer />
       </section>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,103 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Invie from './Invie';
 import registerServiceWorker from './registerServiceWorker';
+import cheet from 'cheet.js';
+import logoPortada from './images/invie.png';
+import acustica from './images/invie-acustica.png';
+import clasica from './images/invie-classic.png';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<Invie />, document.getElementById('root'));
+const initialState = {
+  menu: 
+  [
+    {
+      href: 'index.html',
+      title: 'Home',
+    },
+    {
+      href: '#guitarras',
+      title: 'Guitarras',
+    },
+    {
+      href: 'precios.html',
+      title: 'Precios',
+    }
+  ],
+  logoPortada: {
+    src: logoPortada,
+    alt: 'Invie logotipo'
+  },
+  guitarras: [
+    {
+      image: acustica,
+      alt: 'Guitarra Invie Acústica',
+      name: 'Invie Acústica',
+      features: [
+        'Estilo vintage',
+        'Madera pura',
+        'Incluye estuche invisible de aluminio',
+      ]
+    },
+    {
+      image: clasica,
+      alt: 'Guitarra Invie Classic',
+      name: 'Invie Classic',
+      features: [
+        'Estilo vintage',
+        'Liviana',
+        'Empieza tu camino como Rockstar',
+      ]
+    },
+  ]
+};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'UPDATE_PROPS': {
+      const newProps = action.payload.props;
+      return { ...state, ...newProps};
+    }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, initialState);
+
+const easter = {
+  menu: [
+    {
+      href: 'index.html',
+      title: 'Home',
+    },
+  ]
+}
+
+cheet('i n v i e', () => {
+  /*console.log('Easter Egg!! :D');*/
+  store.dispatch({
+    type: 'UPDATE_PROPS',
+    payload: {
+      props: easter
+    }
+  });
+});
+
+cheet('g o b a c k', () => {
+  /*console.log('Regresaste al estado inicial');*/
+  store.dispatch({
+    type: 'UPDATE_PROPS',
+    payload: {
+      props: initialState
+    }
+  });
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Invie />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
